45ec583701ccf402505b636e3243968ed9bea104
ingeniero26 <ingjerson2014@gmail.com>
2024-11-13 10:57:09 -0500

    conexion a base de datos, entidades pais y marcas

---
+++ OrderTest/OrderTest.Backend/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderTest.Shared.Entities;
+
+namespace OrderTest.Backend.Data
+{
+    // conexion a base de datos
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> countries { get; set; }
+        public DbSet<Brand> brands { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
---
+++ OrderTest/OrderTest.Shared/Entities/Brand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderTest.Shared.Entities
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Marcas")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
